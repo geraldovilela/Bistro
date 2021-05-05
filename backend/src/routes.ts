5985c4f0dd59826869c9ac4619c94e18587b4e0f
Geraldo <geraldovilela19@gmail.com>
2021-05-05 18:42:48 -0400

    dev: ajuste rotas;

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,6 +5,7 @@ import ClapRegisterService from './services/ClapRegisterService'
 
 export async function RegisterClap(request:Request, response:Response ){
     const {IdUser, count, IdArtigo} = request.body;
+    
     const clap= await ClapRegisterService.execute({IdUser,count, IdArtigo});
 
     return response.status(200).json(clap);
