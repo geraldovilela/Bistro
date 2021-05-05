0e70b643b5c41c7c9818f4a8e8be44bf672dd731
Geraldo <geraldovilela19@gmail.com>
2021-05-04 23:06:02 -0400

    dev: setting axios and data persistence;

--- clapme/pages/components/ClapButton/Clap.tsx
+++ clapme/pages/components/ClapButton/Clap.tsx
@@ -1,21 +1,36 @@
-import React, { useState, useCallback } from "react"
-
-import { Container, ButtonContainer } from './styled';
+import React, { useState, useCallback, useEffect } from "react"
+import axios from 'axios';
+import { Container, ButtonContainer,PopupContainer } from './styled';
 import clap from '../../../public/clap.svg'
 
-export default function Clap() {
+interface DataClap {
+    idUser:number;
+    count: number;
+    idArtigo:string;
+}
+
+const Clap: React.FC = ()=>{
     const [count, setCount] = useState(0);
-
-
+    const [show, setShow] = useState(false);
+  
     const eventHandler= ()=>{
-        if(count <= 49) {
-            setCount(count+1)
-        }
+        setShow(!show)        
+        if(count <= 49)
+        { 
+        setCount(count+1)            
+        }   
+    }
+
+    async function SaveData(data:DataClap) {
+        const response = await axios.post('localhost:3000', data)
     }
 
 
     return (
         <Container>
+            <PopupContainer isVisible={show}>
+                <p>{ `+ ${count}`}</p>
+            </PopupContainer>
             <ButtonContainer>
                 <button onClick={eventHandler}>
                     <img src={clap}></img>
@@ -25,3 +40,4 @@ export default function Clap() {
     )
 
 }
+export default Clap;
--- clapme/pages/components/ClapButton/styled.tsx
+++ clapme/pages/components/ClapButton/styled.tsx
@@ -1,9 +1,56 @@
 import styled from 'styled-components'
 
+interface cssProps {
+    isVisible: boolean
+}
+
 export const Container = styled.div`
     display:flex;
     align-items: center;
     justify-content: center;
+`
+
+export const PopupContainer = styled('div')<cssProps>`
+    width:100px;
+    height:100px;
+    position: fixed;    
+    font-family: Arial, Helvetica, sans-serif;
+    z-index: 1;
+    -webkit-transition: opacity 400ms ease-in;
+    -moz-transition: opacity 400ms ease-in;
+    transition: opacity 400ms ease-in;
+    pointer-events: none;
+    opacity: ${({isVisible})=>isVisible ? 1:0};
+    display:flex;
+    justify-content:left;
+    
+    p {
+        display:flex;
+        width: 45px;
+        height: 45px;
+        background-color: #555;
+        color: #fff;
+        text-align: center;
+        border-radius: 50%;
+        position: absolute;
+        bottom: 85%;
+        left: -15%;
+        margin-left: 65px;
+        align-items:center;
+        justify-content:center;
+        justify-items:center;
+               
+        &:after{
+            content: "";
+            position: absolute;
+            top: 99%;
+            left: 50%;
+            margin-left: -5px;
+            border-width: 5px;
+            border-style: solid;
+            border-color: #555 transparent transparent transparent;
+        }
+    }
 `
 
 export const ButtonContainer = styled.div`
@@ -13,12 +60,18 @@ display:flex;
     align-items:center;
     justify-content: center;
     button {
-        width:60px;
-        height:60px;
+        width:90px;
+        height:90px;
         border-radius:50%;
+        box-shadow: 0 0 10px 10px rgba(0 0 0 0.1);
+        img {
+            width:60px;
+            height:60px;
+        }
         &:hover{
             background:grey;
             border-color:rgba(95,50,50, 0.8);
         }
+        
     }  
 `
