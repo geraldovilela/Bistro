78a99318cdb305cad0f4e28a7abd99d30ddc3979
Geraldo <geraldovilela19@gmail.com>
2021-05-05 23:24:56 -0400

    dev: remoção debounce;

--- clapme/pages/components/ClapButton/Clap.tsx
+++ clapme/pages/components/ClapButton/Clap.tsx
@@ -1,51 +1,48 @@
 import React, { useState, useCallback, useEffect } from "react"
 import axios from 'axios';
-import { Container, ButtonContainer,PopupContainer } from './styled';
-import {v4 as uuid} from 'uuid';
+import { Container, ButtonContainer, PopupContainer } from './styled';
+import { v4 as uuid } from 'uuid';
 import useDebounce from "../../Hooks/useDebounce"
 import clap from '../../../public/clap.svg'
 
 
 interface DataClap {
-    IdUser:number;
+    IdUser: number;
     count: number;
-    IdArtigo:string;
+    IdArtigo: string;
 }
 
-const Clap: React.FC = ()=>{
+const Clap: React.FC = () => {
     const [count, setCount] = useState(0);
     const [show, setShow] = useState(false);
-    const IdArtigo=uuid()
-    
-    const eventHandler= ()=>{
-        setShow(!show)        
-        if(count <= 49)
-        { 
-            setCount(count+1)            
-        }   
+    const IdArtigo = uuid()
+
+    const eventHandler = () => {
+        setShow(!show)
+        if (count <= 49) {
+            setCount(count + 1)
+        }
     }
-    
-    async function saveData(data:DataClap) {
-        await axios.post('http://localhost:3333/', data).then(({data})=>{
-            console.log(data);
+
+    async function saveData(data: DataClap) {
+        const resposne = await axios.post('http://localhost:3333/', data)
+    }
+   // const debouncedChange = useDebounce(eventHandler, 300)
+
+    useEffect(() => {
+        const IdUser = Math.floor(Math.random() * 10)
+
+        saveData({
+            count: count,
+            IdArtigo,
+            IdUser
         })
-    }
-    const debouncedChange = useDebounce(saveData, 500)
-    
-    useEffect(()=>{
-    
-    const IdUser=Math.floor(Math.random()*10)            
-        
-    debouncedChange({count:count,
-                 IdArtigo,
-                 IdUser
-        })  
-    },[count])
+    }, [count])
 
     return (
         <Container>
             <PopupContainer isVisible={show}>
-                <p>{ `+ ${count}`}</p>
+                <p>{`+ ${count}`}</p>
             </PopupContainer>
             <ButtonContainer>
                 <button onClick={eventHandler}>
